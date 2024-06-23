48eac85e498d98c8a9f90eef00e3469a604a5fe3
Satpal Singh <satpalsingh248007@gmail.com>
2024-06-23 22:22:23 +0530

    updated

--- server/index.js
+++ server/index.js
@@ -55,9 +55,9 @@ app.post("/login", async (req, res) => {
 });
 
 // Serve the React app for all other routes
-app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../index.html"));
-});
+// app.get("*", (req, res) => {
+//     res.sendFile(path.join(__dirname, "../index.html"));
+// });
 
 app.listen(3000, () => {
     console.log("Server is running on port 3000");
