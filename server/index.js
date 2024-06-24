83548dee2393df8ea481896007b54f815c0b0859
satpalsingh007 <90893688+satpalsingh007@users.noreply.github.com>
2024-06-24 12:50:47 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -7,7 +7,12 @@ const path = require("path");
 // Middleware to parse JSON bodies
 app.use(express.json());
 
-app.use(cors());
+app.use(cors({
+    origin:[],
+    methods:["POST", "GET"],
+    credentials: true    
+}
+    ));
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Headers", "Content-Type");
     next();
@@ -55,9 +60,9 @@ app.post("/login", async (req, res) => {
 });
 
 // Serve the React app for all other routes
-// app.get("*", (req, res) => {
-//     res.sendFile(path.join(__dirname, "../index.html"));
-// });
+app.get("/", (req, res) => {
+    res.json("Hello");
+});
 
 app.listen(3000, () => {
     console.log("Server is running on port 3000");
