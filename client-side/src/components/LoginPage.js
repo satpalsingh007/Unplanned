b6a0dbddee23512353b7c1f361f876f7af93626c
satpalsingh007 <90893688+satpalsingh007@users.noreply.github.com>
2024-06-24 13:53:42 +0530

    Update LoginPage.js

--- client-side/src/components/LoginPage.js
+++ client-side/src/components/LoginPage.js
@@ -11,7 +11,7 @@ const LoginPage = () => {
     const handleSubmit = async (e) => {
         e.preventDefault();
         try {
-            const res = await axios.post("http://localhost:3000/login", {
+            const res = await axios.post("https://unplanned-server.vercel.app/login", {
                 email,
                 password,
             });
