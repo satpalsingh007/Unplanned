6316c73101fa490e4620e09fdd2e2a08389235d1
Satpal Singh <satpalsingh248007@gmail.com>
2024-06-24 14:50:05 +0530

    added homePage

--- client-side/src/components/HomePage.js
+++ client-side/src/components/HomePage.js
@@ -1,7 +1,21 @@
 const HomePage = () => {
     return (
         <>
-            <div>balle balle</div>
+            <h2>What is Unplanned</h2>
+            <div>
+                Unplanned is a website which can be used for finding your next
+                destination with the help of our
+                <ul>Travel Guide Bot - "DESTINA" and an in built Map</ul>
+                <ul>An in built Map with with Routing feature</ul>
+                and you're good to go.
+            </div>
+            <h2>NOTE:</h2>
+            <div>
+                This app is currently in development but you can signup nad
+                login to checkout the Map and Destina the travel guide bot
+                features 😀
+            </div>
+            <h2>THANK-YOU</h2>
         </>
     );
 };
